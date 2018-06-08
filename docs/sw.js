9b410034550a74a4fd2f5516e968f9cb0c56772e
helloworldwriter <writer@helloworldwriter.com>
2018-06-08 21:49:04 +0300

    Initial commit

---
+++ docs/sw.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "4e073d7e8024b77efcc589e81bfb494b"
+  },
+  {
+    "url": "styles/main.css",
+    "revision": "fb00824ac738386b33d5a6ec67f3fa6e"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "globDirectory": "./docs",
+    "globPatterns": [
+        "*.html",
+        "styles/*.css"
+    ],
+    "swDest": "./docs/sw.js"
+};
+
